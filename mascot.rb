7f914571b5c5cd7a70725050839802310acfe089
Michael Glaser <m.glaser82@gmail.com>
2017-12-04 15:09:56 -0800

    Adds case statement in #mascot_sign_for to select the appropriate sign.

--- mascot.rb
+++ mascot.rb
@@ -1,12 +1,24 @@
 # Get input from the user:  the cheer
 def call_out_cheer
   cheer = gets.chomp
-  # return cheer
 end
 
 # Determine the mascot's response based on the argument
 # passed to the method
 def mascot_sign_for(input)
+  called_cheer = input
+  case called_cheer
+    when "RED HOT"
+      return "H-O-T!"
+    when "DO IT AGAIN"
+      return "Go, Fight, Win"
+    when "2 BITS"
+      return "Holler!"
+    when "STOMP YOUR FEET"
+      return "STOMP!"
+    else
+      return "Go Team!"
+  end
 end
 
 # Print the argument passed to the method
